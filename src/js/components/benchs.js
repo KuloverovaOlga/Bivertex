b03d0fbaa31e143a91dfb6b2e8df061263b3ffc3
MonstersBY <monsters.alex0@gmail.com>
2024-02-16 11:36:42 +0300

    feat: our machines

---
+++ src/js/components/benchs.js
@@ -0,0 +1,7 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+import { rem } from '../utils/constants';
+
+document.addEventListener('DOMContentLoaded', () => {
+});
---
+++ src/js/dev/monsters.js
@@ -0,0 +1,2 @@
+import "../components/our_machines.js";
+import "../components/benchs.js";
