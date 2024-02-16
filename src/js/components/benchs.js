9d1f26f255835a418c99ca46ab68b528a2de1f3c
MonstersBY <monsters.alex0@gmail.com>
2024-02-16 12:28:35 +0300

    feat: add some benchs

--- src/js/components/benchs.js
+++ src/js/components/benchs.js
@@ -1,7 +1,22 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
+import $ from "jquery";
+
 import { rem } from '../utils/constants';
 
 document.addEventListener('DOMContentLoaded', () => {
+    $('.switcher__btn').on('click', function (evt) {
+        console.log(123);
+        evt.preventDefault();
+        let $this = $(this);
+        let index = $this.index();
+        let $switcher = $this.closest('.switcher');
+        let $switcher_container = $switcher.find('.switcher__container');
+        let $switcher_contents = $switcher_container.find('.switcher__content');
+        $this.siblings('.switcher__btn').removeClass('active');
+        $this.addClass('active');
+        $switcher_contents.removeClass('active')
+        $($switcher_contents[index]).addClass('active');
+    })
 });
