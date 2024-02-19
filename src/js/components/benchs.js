85c85ed222b2eda42e00348d19fe2f9670787199
MonstersBY <monsters.alex0@gmail.com>
2024-02-19 13:52:17 +0300

    feat: benchs and change fancybox

--- src/js/components/benchs.js
+++ src/js/components/benchs.js
@@ -7,7 +7,6 @@ import { rem } from '../utils/constants';
 
 document.addEventListener('DOMContentLoaded', () => {
     $('.switcher__btn').on('click', function (evt) {
-        console.log(123);
         evt.preventDefault();
         let $this = $(this);
         let index = $this.index();
@@ -19,4 +18,44 @@ document.addEventListener('DOMContentLoaded', () => {
         $switcher_contents.removeClass('active')
         $($switcher_contents[index]).addClass('active');
     })
+    $('.benchs__desk-img').on('click', function (evt) {
+        evt.preventDefault();
+        let $this = $(this);
+        let index = $this.index();
+        let $switcher = $this.closest('.benchs__desk-container');
+        let $switcher_container = $switcher.find('.benchs__desk-center');
+        let $switcher_contents = $switcher_container.find('.benchs__desk-big');
+        $this.siblings('.benchs__desk-img').removeClass('active');
+        $this.addClass('active');
+        $switcher_contents.removeClass('active')
+        $($switcher_contents[index]).addClass('active');
+    })
+
+    const benchs__btns = new Swiper('.benchs__btns', {
+        slidesPerView: 'auto',
+        spaceBetween: rem(1.2),
+        navigation: {
+            nextEl: ".benchs__swiper--right",
+            prevEl: ".benchs__swiper--left",
+        },
+    });
+
+    $(".benchs__mob-selector--top").on("click", function () {
+        $('.benchs__mob-selector--bottom').slideToggle()
+    });
+    $(".benchs__mob-selector--item").on("click", function () {
+        $('.benchs__mob-selector--item').removeClass('active')
+        $(this).addClass('active')
+        let index = $(this).index();
+        $(".benchs__mob-selector--top").find('span').text($(this).text())
+        $('.enchs__mob-content').removeClass('active')
+        $($('.enchs__mob-content')[index]).addClass('active');
+        // Swithc ADD!
+        $('.benchs__mob-selector--bottom').slideToggle()
+    });
+
+    const benchs__mob_swiper = new Swiper('.benchs__mob-swiper', {
+        slidesPerView: 1,
+    });
+
 });
